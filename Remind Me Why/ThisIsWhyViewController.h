d11794a1de6d9da78815388b7a6ae5382e2e6401
Justin Lennox <justinanthonylennox@gmail.com>
2015-04-05 21:49:27 -0400

    inital setup

---
+++ Remind Me Why/ThisIsWhyViewController.h
@@ -0,0 +1,13 @@
+//
+//  ThisIsWhyViewController.h
+//  Remind Me Why
+//
+//  Created by Justin Lennox on 4/5/15.
+//  Copyright (c) 2015 Justin Lennox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ThisIsWhyViewController : UIViewController
+
+@end
