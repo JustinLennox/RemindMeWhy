52ecbad344af89e987474219b09b6d74c81aaf55
Justin Lennox <justinanthonylennox@gmail.com>
2015-04-05 20:25:07 -0400

    Initial Commit

---
+++ Remind Me Why/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Remind Me Why
+//
+//  Created by Justin Lennox on 4/5/15.
+//  Copyright (c) 2015 Justin Lennox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Remind Me Why/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Remind Me Why
+//
+//  Created by Justin Lennox on 4/5/15.
+//  Copyright (c) 2015 Justin Lennox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
